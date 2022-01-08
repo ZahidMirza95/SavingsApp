c1fe31b7e92a3ca72d863940df0aadaf257f2646
Zamzam95-Coder <zam19@rogers.com>
2022-01-07 20:04:10 -0500

    Revert "added graphs, they display but I need to modify them with the transactions info"
    This reverts commit 4d51939e505c1634ad2084bc353314334f0f7e8f.

--- src/pages/StatsPage.js
+++ src/pages/StatsPage.js
@@ -2,48 +2,6 @@ import React from "react";
 import '../App.css';
 import NavBar from "../components/NavBar";
 import { useLocation } from "react-router-dom";
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend,
-  } from 'chart.js';
-  import { Bar } from 'react-chartjs-2';
-  
-  ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
-  );
-  
-  export const options = {
-    responsive: true,
-    plugins: {
-      title: {
-        display: true,
-        text: "Stats: " + "HERE",
-      },
-    },
-  };
-  
-  const labels = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  
-  export const data = {
-    labels,
-    datasets: [
-      {
-        label: 'Dataset 1',
-        data: labels.map(() => 1),
-        backgroundColor: 'black',
-      }
-    ],
-  };
 
 function StatsPage() {
     const location = useLocation();
@@ -53,10 +11,6 @@ function StatsPage() {
         <div className="App">
             <NavBar balance = {balance} transactionsDict={transactionsDict}/>
             <h1> {balance} </h1>
-            <Bar
-            options={options}
-            data={data}
-            />
         </div>
     );
 }
