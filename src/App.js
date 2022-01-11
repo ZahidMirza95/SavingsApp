1cfc18c5efd037a57e4b2a1fdbbfd698c4c45c8b
Zamzam95-Coder <zam19@rogers.com>
2022-01-10 19:56:40 -0500

    changed browserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import './App.css';
 import AchievementsPage from './pages/AchievementsPage';
 import HomePage from './pages/HomePage';
 import StatsPage from './pages/StatsPage';
-import { BrowserRouter, Route, Routes, Switch } from 'react-router-dom';
+import { HashRouter, Link, BrowserRouter, Route, Routes, Switch } from 'react-router-dom';
 
 function App() {
   return (
-  <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route exact path = "/" element = {<HomePage/>}/>
-          <Route path = "/achievements" element = {<AchievementsPage/>}/>
-          <Route path = "/stats" element = {<StatsPage/>}/>
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <HashRouter basename={process.env.PUBLIC_URL}>
+          <div className="App">
+          <Routes>
+            <Route exact path = "/" element = {<HomePage/>}/>
+            <Route path = "/achievements" element = {<AchievementsPage/>}/>
+            <Route path = "/stats" element = {<StatsPage/>}/>
+          </Routes>
+          </div>
+    </HashRouter>
   );
 }
 
